5855996dfb470141441ca0b3945655d011dffd68
Forkbabu <sayantandas30011998@gmail.com>
2020-06-11 17:08:08 +0530

    Update scattnet.py

--- SCAttNet/scattnet.py
+++ SCAttNet/scattnet.py
@@ -19,22 +19,22 @@ def channel_spatial_block(input_feature, name, ratio=8):
     As described in https://arxiv.org/abs/1807.06521.
     """
 
-    with tf.variable_scope(name):
+    with tf.compat.v1.variable_scope(name):
         scale,attention_feature = channel_attention(input_feature, 'ch_at', ratio)
         concat,attention_feature = spatial_attention(attention_feature, 'sp_at')
     return attention_feature
 
 
 def channel_attention(input_feature, name, ratio=8):
-    kernel_initializer = tf.contrib.layers.variance_scaling_initializer()
-    bias_initializer = tf.constant_initializer(value=0.0)
+    kernel_initializer = tf.compat.v1.keras.initializers.VarianceScaling(scale=2.0)
+    bias_initializer = tf.compat.v1.constant_initializer(value=0.0)
 
-    with tf.variable_scope(name):
+    with tf.compat.v1.variable_scope(name):
         channel = input_feature.get_shape()[-1]
-        avg_pool = tf.reduce_mean(input_feature, axis=[1, 2], keep_dims=True)
+        avg_pool = tf.reduce_mean(input_tensor=input_feature, axis=[1, 2], keepdims=True)
 
         assert avg_pool.get_shape()[1:] == (1, 1, channel)
-        avg_pool = tf.layers.dense(inputs=avg_pool,
+        avg_pool = tf.compat.v1.layers.dense(inputs=avg_pool,
                                    units=channel // ratio,
                                    activation=tf.nn.relu,
                                    kernel_initializer=kernel_initializer,
@@ -42,7 +42,7 @@ def channel_attention(input_feature, name, ratio=8):
                                    name='mlp_0',
                                    reuse=None)
         assert avg_pool.get_shape()[1:] == (1, 1, channel // ratio)
-        avg_pool = tf.layers.dense(inputs=avg_pool,
+        avg_pool = tf.compat.v1.layers.dense(inputs=avg_pool,
                                    units=channel,
                                    kernel_initializer=kernel_initializer,
                                    bias_initializer=bias_initializer,
@@ -50,15 +50,15 @@ def channel_attention(input_feature, name, ratio=8):
                                    reuse=None)
         assert avg_pool.get_shape()[1:] == (1, 1, channel)
 
-        max_pool = tf.reduce_max(input_feature, axis=[1, 2], keep_dims=True)
+        max_pool = tf.reduce_max(input_tensor=input_feature, axis=[1, 2], keepdims=True)
         assert max_pool.get_shape()[1:] == (1, 1, channel)
-        max_pool = tf.layers.dense(inputs=max_pool,
+        max_pool = tf.compat.v1.layers.dense(inputs=max_pool,
                                    units=channel // ratio,
                                    activation=tf.nn.relu,
                                    name='mlp_0',
                                    reuse=True)
         assert max_pool.get_shape()[1:] == (1, 1, channel // ratio)
-        max_pool = tf.layers.dense(inputs=max_pool,
+        max_pool = tf.compat.v1.layers.dense(inputs=max_pool,
                                    units=channel,
                                    name='mlp_1',
                                    reuse=True)
@@ -71,16 +71,16 @@ def channel_attention(input_feature, name, ratio=8):
 
 def spatial_attention(input_feature, name):
     kernel_size = 7
-    kernel_initializer = tf.contrib.layers.variance_scaling_initializer()
-    with tf.variable_scope(name):
-        avg_pool = tf.reduce_mean(input_feature, axis=[3], keep_dims=True)
+    kernel_initializer = tf.compat.v1.keras.initializers.VarianceScaling(scale=2.0)
+    with tf.compat.v1.variable_scope(name):
+        avg_pool = tf.reduce_mean(input_tensor=input_feature, axis=[3], keepdims=True)
         assert avg_pool.get_shape()[-1] == 1
-        max_pool = tf.reduce_max(input_feature, axis=[3], keep_dims=True)
+        max_pool = tf.reduce_max(input_tensor=input_feature, axis=[3], keepdims=True)
         assert max_pool.get_shape()[-1] == 1
         concat = tf.concat([avg_pool, max_pool], 3)
         assert concat.get_shape()[-1] == 2
 
-        concat = tf.layers.conv2d(concat,
+        concat = tf.compat.v1.layers.conv2d(concat,
                                   filters=1,
                                   kernel_size=[kernel_size, kernel_size],
                                   strides=[1, 1],
@@ -98,7 +98,7 @@ def spatial_attention(input_feature, name):
 
 def n_enc_block(inputs, phase_train, n, k, name):
     h = inputs
-    with tf.variable_scope(name):
+    with tf.compat.v1.variable_scope(name):
         for i in range(n):
             h = conv2d(h, k, 3, stride=1, name='conv_{}'.format(i + 1))
             h = batch_norm(h, phase_train, name='bn_{}'.format(i + 1))
@@ -109,7 +109,7 @@ def n_enc_block(inputs, phase_train, n, k, name):
 
 
 def encoder(inputs, phase_train, name='encoder'):
-    with tf.variable_scope(name):
+    with tf.compat.v1.variable_scope(name):
         h, mask_1 = n_enc_block(inputs, phase_train, n=2, k=64, name='block_1')
         h, mask_2 = n_enc_block(h, phase_train, n=2, k=128, name='block_2')
         h, mask_3 = n_enc_block(h, phase_train, n=3, k=256, name='block_3')
@@ -120,7 +120,7 @@ def encoder(inputs, phase_train, name='encoder'):
 
 def n_dec_block(inputs, mask, adj_k, phase_train, n, k, name):
     in_shape = inputs.get_shape().as_list()
-    with tf.variable_scope(name):
+    with tf.compat.v1.variable_scope(name):
         h = maxunpool2d(inputs, mask, name='unpool')
         for i in range(n):
             if i == (n - 1) and adj_k:
@@ -133,13 +133,13 @@ def n_dec_block(inputs, mask, adj_k, phase_train, n, k, name):
     return h
 
 def dec_last_conv(inputs, phase_train, k, name):
-    with tf.variable_scope(name):
+    with tf.compat.v1.variable_scope(name):
         h = conv2d(inputs, k, 1, name='conv')
     return h
 
 
 def decoder(inputs, mask, phase_train, name='decoder'):
-    with tf.variable_scope(name):
+    with tf.compat.v1.variable_scope(name):
 
         h = n_dec_block(inputs, mask[0], False, phase_train, n=3, k=512, name='block_5')
 
@@ -158,7 +158,7 @@ def decoder(inputs, mask, phase_train, name='decoder'):
     return logits
 
 def inference(inputs, phase_train):
-    with tf.variable_scope('segnet'):
+    with tf.compat.v1.variable_scope('segnet'):
         h, mask = encoder(inputs, phase_train, name='encoder')
         logits = decoder(h, mask, phase_train, name='decoder')
     return logits
